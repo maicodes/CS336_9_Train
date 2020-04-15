161fe79d0c3b6092a69f78d9f1293cbd76c36a8a
bakedziti88 <dw523@scarletmail.rutgers.edu>
2020-04-15 13:10:11 -0400

    Add files via upload

---
+++ CS336_g9/src/pkg/Models/StationTrain.java
@@ -0,0 +1,37 @@
+package pkg.Models;
+
+public class StationTrain {
+	private int id;
+	private String dep, arr, origin, dest;
+	
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDest() {
+		return dest;
+	}
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDep() {
+		return dep;
+	}
+	public void setDep(String dep) {
+		this.dep = dep;
+	}
+	public String getArr() {
+		return arr;
+	}
+	public void setArr(String arr) {
+		this.arr = arr;
+	}
+}
